5c4f93d2a1bd3fd3fd3f07b7c56fe8a9cf38bf36
AtipatD <atipat_d@cmu.ac.th>
2021-11-26 16:05:21 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,22 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    
+    int userInt = 1, countOdd = 0, countEven = 0;
+    while(userInt != 0){
+        cout << "Enter an integer: ";
+        cin >> userInt;
+        
+        if(userInt != 0){
+            if(userInt%2 == 0){
+                countEven++;
+            }else{
+                countOdd++;
+            }
+        }
+    }
+    cout << "#Even numbers = " << countEven << endl;
+    cout << "#Odd numbers = " << countOdd << endl;
+
     return 0;
 }
